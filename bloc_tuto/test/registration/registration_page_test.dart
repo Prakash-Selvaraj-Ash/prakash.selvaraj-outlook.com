760e8840143947b8383d91f9564a3bf79c3a122c
prakash <prakash.selvaraj@outlook.com>
2020-05-22 11:23:24 +0530

    test cases added

--- bloc_tuto/test/registration/registration_page_test.dart
+++ bloc_tuto/test/registration/registration_page_test.dart
@@ -42,14 +42,70 @@ void main() {
       verify(
         bloc.add(UserNameChanged(userName)),
       ).called(1);
+    });
+  });
+
+  group('Registration Page - password Changed', () {
+    setUp(() {
+      bloc = MockRegistrationBloc();
+    });
+
+    testWidgets('password changed', (WidgetTester tester) async {
+      const String password = 'password';
+      RegistrationPage page = RegistrationPage();
+      when(bloc.add(PasswordChanged(password))).thenAnswer((_) =>
+          RegistrationModelChanged(RegistrationModel.empty()
+              .copyWith(isValid: true, password: password)));
+
+      await tester.pumpWidget(BlocProvider.value(
+          value: bloc,
+          child: MaterialApp(
+            home: page,
+          )));
 
       await tester.pumpAndSettle();
 
-      var buttonFinder = find.byType(RaisedButton);
-      expect(buttonFinder, findsOneWidget);
+      var finder = find.byType(TextField);
 
-      var button = tester.widget<RaisedButton>(buttonFinder);
-      expect(button.enabled, true);
+      expect(finder, findsNWidgets(3));
+
+      await tester.enterText(finder.at(1), password);
+
+      verify(
+        bloc.add(PasswordChanged(password)),
+      ).called(1);
+    });
+  });
+
+  group('Registration Page - confirm password Changed', () {
+    setUp(() {
+      bloc = MockRegistrationBloc();
+    });
+
+    testWidgets('confirm password changed', (WidgetTester tester) async {
+      const String confirmPassword = 'confirmPassword';
+      RegistrationPage page = RegistrationPage();
+      when(bloc.add(ConfirmPasswordChanged(confirmPassword))).thenAnswer((_) =>
+          RegistrationModelChanged(RegistrationModel.empty()
+              .copyWith(isValid: true, confirmPassword: confirmPassword)));
+
+      await tester.pumpWidget(BlocProvider.value(
+          value: bloc,
+          child: MaterialApp(
+            home: page,
+          )));
+
+      await tester.pumpAndSettle();
+
+      var finder = find.byType(TextField);
+
+      expect(finder, findsNWidgets(3));
+
+      await tester.enterText(finder.at(2), confirmPassword);
+
+      verify(
+        bloc.add(ConfirmPasswordChanged(confirmPassword)),
+      ).called(1);
     });
   });
 }
